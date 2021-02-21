0f4c9149bb0bc0aae73e68313a06bc4aec11a28e
Vebjørn Gilberg <vebjorngilberg@gmail.com>
2021-02-21 23:11:19 +0100

    added schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,34 +1,48 @@
-import { ApolloServer, gql } from 'apollo-server'
+//import { ApolloServer, gql } from 'apollo-server'
+
+const { ApolloServer, gql } = require('apollo-server');
+//import { typeDefs } from 'schema.graphql'
 
 
-//The typeDefs constant defines the GraphQL schema. 
-//Here, it defines a simple Query type with one field called info. This field has the type String!. 
-//The exclamation mark in the type definition means that this field is required and can never be null.
 
-const typeDefs = gql`
-    type Query {
-        info: String!
-    }
-`;
 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+}]
 
 //The resolvers object is the actual implementation of the GraphQL schema.
 
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+
+    Link: {
+        id: parent => parent.id,
+        description: parent => parent.description,
+        url: parent => parent.url,
     }
 }
 
 //Finally, the schema and resolvers are bundled and passed to ApolloServer which is imported from apollo-server. 
 //This tells the server what API operations are accepted and how they should be resolved.
 
+const fs = require('fs')
+const path = require('path')
+
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'), 
+        'utf8'
+    ),
+    //typeDefs,
     resolvers,
 })
 
-server.listen.then(({ url }) => 
+server.listen().then(({ url }) => 
     console.log(`
                 🚀 Server is running...
                 Listening on port 4000 at ${url}
